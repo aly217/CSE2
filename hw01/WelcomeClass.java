be56c6f4bc4f081baf538d7de8403fdcacf29b0c
aly217 <aly217@lehigh.edu>
2014-08-31 20:19:08 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+//Anjela Yates
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java Welcome Class//
+
+//define class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args){
+        //print the statement
+        System.out.println("---------------");
+        System.out.println("    Welcome    ");
+        System.out.println("---------------");
+        System.out.println("  ^  ^  ^  ^  ^  ^ ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("-a--l--y--2--1--7--");
+        System.out.println(" \\/ \\/ \\/ \\/ \\/ \\/ ");
+        System.out.println(" v   v   v   v   v   v  ");
+        System.out.println("Being an orientation leader this fall was the best experience ever!");
+        System.out.println("I enjoyed getting to know all my first-years.");
+        
+        //test
+        
+    }
+}
